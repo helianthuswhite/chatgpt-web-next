f7932d075f6cfc7b7cd3f6bd73ad04ff4e6384cd
helianthuswhite <hyz19960229@gmail.com>
2023-05-10 21:51:35 +0800

    🔧 fix build error

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -24,7 +24,7 @@ export default dynamic(
                                 email: userInfo.email,
                                 nickName: userInfo.nickName,
                             });
-                            onClick?.(e);
+                            onClick?.(e as any);
                         }}
                     />
                 );
